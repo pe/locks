62fc6053df77b4bf7f8d288c3edb3125f222452c
Philippe Eberli <p.eberli@gmx.ch>
2021-11-02 20:55:55 +0100

    inline & extract m

--- src/main/java/locks/Locks.java
+++ src/main/java/locks/Locks.java
@@ -6,13 +6,10 @@ import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.teeing;
 
 import one.util.streamex.AbstractStreamEx;
-import one.util.streamex.EntryStream;
 import one.util.streamex.MoreCollectors;
 import one.util.streamex.StreamEx;
 
 import java.io.InputStream;
-import java.io.PrintStream;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
@@ -27,107 +24,106 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Locks {
-   private static final Pattern PATTERN = Pattern.compile(
-         "([\\d-]+ [\\d:.]+) Df logind\\[\\d+:[\\da-f]+] \\[com.apple.login:Logind_General] -\\[SessionAgent SA_SetSessionStateForUser:state:reply:]:536: state set to: (\\d)\n");
-   private static final DateTimeFormatter SHORT_DATE = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
-   private static final DateTimeFormatter SHORT_TIME = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+    private static final Pattern PATTERN = Pattern.compile(
+          "([\\d-]+ [\\d:.]+) Df logind\\[\\d+:[\\da-f]+] \\[com.apple.login:Logind_General] -\\[SessionAgent SA_SetSessionStateForUser:state:reply:]:536: state set to: (\\d)\n");
+    private static final DateTimeFormatter SHORT_DATE = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+    private static final DateTimeFormatter SHORT_TIME = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
 
-   public static void main(String[] args) {
-      StreamEx<Event> events = toEvents(System.in);
-      events.append(new Event(LocalDateTime.now(), Event.Type.LOCK));
-      StreamEx<Duration> durations = toDurations(events);
-      EntryStream<LocalDate, StreamEx<LocalDateTime>> eventsByDate = groupByDate(durations);
-      print(System.out, eventsByDate);
-   }
+    public static void main(String[] args) {
+        StreamEx<Event> events = toEvents(System.in);
+        events.append(new Event(LocalDateTime.now(), Event.Type.LOCK));
+        StreamEx<Duration> durations = toDurations(events);
+        StreamEx<String> strings = format(durations);
+        strings.forEach(System.out::println);
+    }
 
-   static StreamEx<Event> toEvents(InputStream input) {
-      Stream<MatchResult> matches = new Scanner(input).findAll(PATTERN);
-      return StreamEx.of(matches).map(matcher -> new Event(matcher.group(1), matcher.group(2)));
-   }
+    static StreamEx<Event> toEvents(InputStream input) {
+        Stream<MatchResult> matches = new Scanner(input).findAll(PATTERN);
+        return StreamEx.of(matches).map(matcher -> new Event(matcher.group(1), matcher.group(2)));
+    }
 
-   static StreamEx<Duration> toDurations(StreamEx<Event> events) {
-      return events
-            .dropWhile(Event::isLock)
-            .collapse(Event::bothAreUnlock, selectFirst())
-            .collapse(Event::bothAreLock, selectLast())
-            .collapse(Event::lockAndUnlock, mapping(Event::at, teeing(first(), last(), Duration::new)))
-            .intervalMap((d1, d2) -> MINUTES.between(d1.stop, d2.start) < 15, (d1, d2) -> new Duration(d1.start, d2.stop));
-   }
+    static StreamEx<Duration> toDurations(StreamEx<Event> events) {
+        return events
+              .dropWhile(Event::isLock)
+              .collapse(Event::bothAreUnlock, selectFirst())
+              .collapse(Event::bothAreLock, selectLast())
+              .collapse(Event::lockAndUnlock, mapping(Event::at, teeing(first(), last(), Duration::new)))
+              .intervalMap((d1, d2) -> MINUTES.between(d1.stop, d2.start) < 15, (d1, d2) -> new Duration(d1.start, d2.stop));
+    }
 
-   private static BinaryOperator<Event> selectFirst() {
-      return (u, v) -> u;
-   }
+    private static BinaryOperator<Event> selectFirst() {
+        return (u, v) -> u;
+    }
 
-   private static BinaryOperator<Event> selectLast() {
-      return (u, v) -> v;
-   }
+    private static BinaryOperator<Event> selectLast() {
+        return (u, v) -> v;
+    }
 
-   private static <X> Collector<X, ?, X> first() {
-      return collectingAndThen(MoreCollectors.first(), Optional::get);
-   }
+    private static <X> Collector<X, ?, X> first() {
+        return collectingAndThen(MoreCollectors.first(), Optional::get);
+    }
 
-   private static <X> Collector<X, ?, X> last() {
-      return collectingAndThen(MoreCollectors.last(), Optional::get);
-   }
+    private static <X> Collector<X, ?, X> last() {
+        return collectingAndThen(MoreCollectors.last(), Optional::get);
+    }
 
-   static EntryStream<LocalDate, StreamEx<LocalDateTime>> groupByDate(StreamEx<Duration> durations) {
-      return durations
-            .mapToEntry(d -> d.start.toLocalDate(), d -> StreamEx.of(d.start, d.stop))
-            .collapseKeys(AbstractStreamEx::append);
-   }
+    static StreamEx<String> format(StreamEx<Duration> durations) {
+        return durations
+              .mapToEntry(d -> d.start.toLocalDate(), d -> StreamEx.of(d.start, d.stop))
+              .collapseKeys(AbstractStreamEx::append)
+              .mapKeyValue((day, eventsOfTheDay) ->
+                    day.format(SHORT_DATE) +
+                    "\t\t" +
+                    eventsOfTheDay
+                          .map(event -> event.format(SHORT_TIME))
+                          .collect(Collectors.joining("\t")));
+    }
 
-   static void print(PrintStream out, EntryStream<LocalDate, StreamEx<LocalDateTime>> eventsByDate) {
-      eventsByDate
-            .forKeyValue((day, eventsOfTheDay) -> out.println(day.format(SHORT_DATE) + "\t\t" + eventsOfTheDay
-                  .map(event -> event.format(SHORT_TIME))
-                  .collect(Collectors.joining("\t"))));
-   }
+    record Duration(LocalDateTime start, LocalDateTime stop) {
+    }
 
-   record Duration(LocalDateTime start, LocalDateTime stop) {
-   }
+    record Event(LocalDateTime at, Locks.Event.Type type) {
+        private static final DateTimeFormatter DATE_TIME = new DateTimeFormatterBuilder()
+              .append(DateTimeFormatter.ISO_LOCAL_DATE)
+              .appendLiteral(' ')
+              .append(DateTimeFormatter.ISO_LOCAL_TIME)
+              .appendOffset("+HHmm", "")
+              .toFormatter();
 
-   record Event(LocalDateTime at, Locks.Event.Type type) {
-      private static final DateTimeFormatter DATE_TIME = new DateTimeFormatterBuilder()
-            .append(DateTimeFormatter.ISO_LOCAL_DATE)
-            .appendLiteral(' ')
-            .append(DateTimeFormatter.ISO_LOCAL_TIME)
-            .appendOffset("+HHmm", "")
-            .toFormatter();
+        public Event(String at, String type) {
+            this(LocalDateTime.parse(at, DATE_TIME), Event.Type.of(type));
+        }
 
-      public Event(String at, String type) {
-         this(LocalDateTime.parse(at, DATE_TIME), Event.Type.of(type));
-      }
+        private boolean isLock() {
+            return type == Type.LOCK;
+        }
 
-      private boolean isLock() {
-         return type == Type.LOCK;
-      }
+        private boolean isUnlock() {
+            return type == Type.UNLOCK;
+        }
 
-      private boolean isUnlock() {
-         return type == Type.UNLOCK;
-      }
+        private static boolean bothAreLock(Event event1, Event event2) {
+            return event1.isLock() && event2.isLock();
+        }
 
-      private static boolean bothAreLock(Event event1, Event event2) {
-         return event1.isLock() && event2.isLock();
-      }
+        private static boolean bothAreUnlock(Event event1, Event event2) {
+            return event1.isUnlock() && event2.isUnlock();
+        }
 
-      private static boolean bothAreUnlock(Event event1, Event event2) {
-         return event1.isUnlock() && event2.isUnlock();
-      }
+        private static boolean lockAndUnlock(Event event1, Event event2) {
+            return event1.isUnlock() && event2.isLock();
+        }
 
-      private static boolean lockAndUnlock(Event event1, Event event2) {
-         return event1.isUnlock() && event2.isLock();
-      }
+        enum Type {
+            LOCK, UNLOCK, IGNORED;
 
-      enum Type {
-         LOCK, UNLOCK, IGNORED;
-
-         public static Type of(String type) {
-            return switch (type) {
-               case "2" -> UNLOCK;
-               case "3", "5" -> LOCK;
-               default -> IGNORED;
-            };
-         }
-      }
-   }
+            public static Type of(String type) {
+                return switch (type) {
+                    case "2" -> UNLOCK;
+                    case "3", "5" -> LOCK;
+                    default -> IGNORED;
+                };
+            }
+        }
+    }
 }
