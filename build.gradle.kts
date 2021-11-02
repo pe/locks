652ab244cbc054353d3015b5ddf3171fd5d4f224
Philippe Eberli <p.eberli@gmx.ch>
2021-11-02 20:56:21 +0100

    update junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,7 @@ repositories {
 
 dependencies {
     implementation("one.util:streamex:0.7.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
